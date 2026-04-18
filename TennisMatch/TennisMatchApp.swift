8f3d83d5f249e52a31ca6d82550d86e0207f3243
XUE <22099047r@connect.polyu.hk>
2026-04-18 21:37:20 +0800

    Initial Commit

---
+++ TennisMatch/TennisMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisMatchApp.swift
+//  TennisMatch
+//
+//  Created by XUE on 18/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct TennisMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
