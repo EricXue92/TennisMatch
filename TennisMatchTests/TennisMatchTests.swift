3f6eaf811cc9f3ea956d01abb0d2e3088ca4df0a
XUE <22099047r@connect.polyu.hk>
2026-04-26 18:10:50 +0800

    feat(user): UserProfile.id 持久化(后端友好建模)

--- TennisMatchTests/TennisMatchTests.swift
+++ TennisMatchTests/TennisMatchTests.swift
@@ -6,3 +6,19 @@ final class TennisMatchTests: XCTestCase {
         XCTAssertTrue(true)
     }
 }
+
+extension TennisMatchTests {
+    @MainActor
+    func test_userStore_hasStableID() {
+        let key = "userStore.id"
+        UserDefaults.standard.removeObject(forKey: key)
+
+        let s1 = UserStore()
+        let id1 = s1.id
+
+        let s2 = UserStore()
+        let id2 = s2.id
+
+        XCTAssertEqual(id1, id2, "id 应在 init 间稳定(从 UserDefaults 读)")
+    }
+}
